bc1485726edd9089147603ceda977ee7366fe930
RobBl8ke <RobB@korbicom.com>
2022-02-23 08:47:26 +0200

    Logic for closest pair in two arrays

--- spec/problems/arrays/closest_pair_2_sorted_arrays/closest_pair.js
+++ spec/problems/arrays/closest_pair_2_sorted_arrays/closest_pair.js
@@ -1,5 +1,8 @@
 // Source: https://www.geeksforgeeks.org/given-two-sorted-arrays-number-x-find-pair-whose-sum-closest-x/
 
+// Simple solution is to run two loops. The outer loop considers every element int
+// the first array and the inner loop considers every element in the second array.
+// Keep track of the minimum difference between array1[i], array2[j].
 const closest_pair_unoptimized = (array1, array2, value) => {
 
     let current = -1;
@@ -21,15 +24,37 @@ const closest_pair_unoptimized = (array1, array2, value) => {
     return pair;
 };
 
+/*
+The idea is to start from left side of one array and right side of another array,
+and use the following algorithm:
+
+    1) Initialize a variable diff as infinite (Diff is used to store the 
+    difference between pair and value).  We need to find the minimum diff.
+    2) Initialize two index variables l and r in the given sorted array.
+        (a) Initialize first to the leftmost index in ar1:  l = 0
+        (b) Initialize second  the rightmost index in ar2:  r = n-1
+    3) Loop while  l = 0
+        (a) If  abs(ar1[l] + ar2[r] - sum) < diff  then 
+            update diff and result 
+        (b) If (ar1[l] + ar2[r] <  sum )  then l++
+        (c) Else r--    
+    4) Print the result. 
+*/
 const closest_pair_optimized = (array1, array2, value) => {
     let diff = Number.MAX_VALUE;
-    let leftIndex = 0;
+    let leftIndex = 0; // array1
     let rightIndex = array2.length - 1;
     let leftVal, rightVal;
 
+    console.log('---');
+
     while (leftIndex < array1.length && rightIndex >= 0) {
+        
+        console.log(leftIndex, rightIndex);
+
         let calcValue = array1[leftIndex] + array2[rightIndex];
         let temp = Math.abs(value - calcValue);
+
         if (temp < diff) {
             diff = temp;
             leftVal = array1[leftIndex];
--- spec/problems/arrays/closest_pair_2_sorted_arrays/closest_pair.spec.js
+++ spec/problems/arrays/closest_pair_2_sorted_arrays/closest_pair.spec.js
@@ -19,5 +19,26 @@ describe("Find the closest pair from two sorted arrays", () => {
         it(`should return [7, 40] when given [1, 4, 5, 7] and [10, 20, 30, 40] with a target value of 50`, () => {
             expect(optimized([1, 4, 5, 7] , [10, 20, 30, 40], 50)).toEqual([7, 40]);
         })
+
+        it(`should return [3, 14] when given [1, 2, 3, 4] and [6, 7, 8, 9, 10, 11, 12, 13, 14] with a target value of 17`, () => {
+            expect(optimized([1, 2, 3, 4] , [6, 7, 8, 9, 10, 11, 12, 13, 14], 17)).toEqual([3, 14]);
+        })
+
+        it(`should return [1, 6] when given [1, 2, 3, 4] and [6, 7, 8, 9, 10, 11, 12, 13, 14] with a target value of 7`, () => {
+            expect(optimized([1, 2, 3, 4] , [6, 7, 8, 9, 10, 11, 12, 13, 14], 7)).toEqual([1, 6]);
+        })
+
+        it(`should return [2, 11] when given [1, 2, 3, 4] and [6, 7, 8, 9, 10, 11, 17, 19, 20] with a target value of 12`, () => {
+            expect(optimized([1, 2, 3, 4] , [6, 7, 8, 9, 10, 11, 17, 19, 20], 13)).toEqual([2, 11]);
+        })
+
+        // Edge Case:
+        it(`should return ? when given ? and ? with a target value of ?`, () => {
+            expect(optimized([50] , [6, 7, 8, 9, 10, 11, 17, 19, 20], 30)).toEqual([50, 6]);
+        })
+
+        fit(`should return ? when given ? and ? with a target value of ?`, () => {
+            expect(optimized([24, 26] , [6, 7, 8, 9, 10, 11, 17, 19, 20], 100)).toEqual([50, 26]);
+        })
     });
 });
